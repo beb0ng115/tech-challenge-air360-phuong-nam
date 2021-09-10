b8d70f9f960713848c25161fe325c774e550c725
nguyenchicong <cong.chi@scalefast.com>
2021-09-10 13:51:34 +0700

    add init source

---
+++ src/TechChallenge/TechChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TechChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strInputTests = new List<string>() {
+                "Hello I’m [name] and I’m {age} years old!",
+                "Hello I’m [(firstname) (lastname)] and I’m {age} years old!",
+                "Hello I’m [(firstname) (lastname)] and I’m {age} years old!",
+                "Hello I’m ((firstname) (lastname)) and I’m {age} years old!",
+                "Hello I’m [(firstname) (lastname]) and I’m {age} years old!",
+                "Hello I’m ((firstname) (lastname)and I’m {age} years old!"
+            };
+            /// 
+            /// Do your tech challenge here
+            /// 
+            Console.ReadLine();
+        }
+    }
+}
